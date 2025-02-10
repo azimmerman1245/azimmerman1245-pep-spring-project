af250eb64b294aebf82243fceff902aaef690405
Aalok Zimmerman <azimmerman1245@gmail.com>
2025-02-10 21:49:30 +0000

    Added additional comments on implementation ideas

--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -4,4 +4,5 @@ public class AccountService {
 
     // Use @Service and @Transactional (rollbackFor = specifyException.class) annotations
     // Use @Autowired for singleton instance
+    // Consult Spring JPA CRUD Coding Lab on implementing the Service Class using Optional operations
 }
--- src/main/java/com/example/service/MessageService.java
+++ src/main/java/com/example/service/MessageService.java
@@ -4,4 +4,5 @@ public class MessageService {
 
     // Use @Service and @Transactional (rollbackFor = specifyException.class) annotations
     // Use @Autowired on the singleton instance
+    // Consult Spring JPA CRUD Coding Lab on implementing the Service Class using Optional operations
 }
