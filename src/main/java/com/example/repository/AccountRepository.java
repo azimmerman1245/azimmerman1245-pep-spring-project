d87ea7e9171337e33c70928b7297d7b4ec354c3c
Aalok Zimmerman <azimmerman1245@gmail.com>
2025-02-10 21:37:12 +0000

    AccountRepository now extends JpaRepository and imports the necessary entity Account

--- src/main/java/com/example/repository/AccountRepository.java
+++ src/main/java/com/example/repository/AccountRepository.java
@@ -1,4 +1,12 @@
 package com.example.repository;
 
-public interface AccountRepository {
+import com.example.entity.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface AccountRepository extends JpaRepository<Account, Integer> {
+    
+
 }
