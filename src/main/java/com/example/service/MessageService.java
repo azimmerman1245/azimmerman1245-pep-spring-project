c5e2bc863feb3e25d4eb3012c3b401d9a332e65b
Aalok Zimmerman <azimmerman1245@gmail.com>
2025-02-11 23:06:15 +0000

    Added imports that appear to be necessary for Service class use

--- src/main/java/com/example/service/MessageService.java
+++ src/main/java/com/example/service/MessageService.java
@@ -1,8 +1,27 @@
 package com.example.service;
 
+import com.example.entity.Message;
+import com.example.repository.MessageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class MessageService {
 
     // Use @Service and @Transactional (rollbackFor = specifyException.class) annotations
     // Use @Autowired on the singleton instance
     // Consult Spring JPA CRUD & JPA Multiplicity Coding Labs on implementing the Service Class using Optional operations
+
+    MessageRepository messageRepository;
+    @Autowired
+    public MessageService(MessageRepository messageRepository) {
+        this.messageRepository = messageRepository;
+    }
+
+    public Message persistAccount(Message account) {
+        return messageRepository.save(account);
+    }
 }
