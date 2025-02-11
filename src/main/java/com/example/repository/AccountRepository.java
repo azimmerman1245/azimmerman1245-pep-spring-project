e5c63a554f5b8b64bbf553b94bf93677a012cf0c
Aalok Zimmerman <azimmerman1245@gmail.com>
2025-02-11 21:36:31 +0000

    Added Queries for creating an account and login

--- src/main/java/com/example/repository/AccountRepository.java
+++ src/main/java/com/example/repository/AccountRepository.java
@@ -11,4 +11,26 @@ public interface AccountRepository extends JpaRepository<Account, Integer> {
     // Use @Query to write Query statements with :variable as a parametrized variable that can be
     // set using @Param("variable") DataType variable;
     // Use named Queries? Check Named Queries Coding Challenge
+
+    /**
+     * This Query inserts an account with a given username and password
+     * 
+     * @param username given username
+     * @param password given password
+     * @return the account created
+     */
+
+     @Query("INSERT INTO account (username, password) VALUES (:usernameVar, :passwordVar)")
+     Account createAccount(@Param("usernameVar") String username, @Param("passwordVar") String password);
+
+     /**
+      * This Query retrieves an account from the database with a givern username and password
+      * @param username given username
+      * @param password given password
+      * @return an account if username and password match, or null if they do not
+      */
+
+      @Query("SELECT * FROM account WHERE username = :usernameVar AND password = :passwordVar")
+      Account loginAccount(@Param("usernameVar") String username, @Param("passwordVar") String password);
+
 }
